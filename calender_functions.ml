7b5b0768c87cb3507eba134947ca214ae2ca5d26
Kingshuk Mondal <141356181+Kings-Mon@users.noreply.github.com>
2024-04-14 11:25:07 +0530

    calender_functions.ml

--- calender_functions.ml
+++ calender_functions.ml
@@ -6,16 +6,15 @@ type date = { dayofmonth:int; monthnum:int; year:int }
 (*1. Write a function date2daynum: date -> int which takes a date and returns the 
 corresponding day number of the year it is. Your code should take into consideration 
 whether the year in question is a leap year or not. *)
-let is_leap_year year =
-  (year mod 4 = 0 && year mod 100 <> 0) || (year mod 400 = 0)
+
+let is_leap_year year = (year mod 4 = 0 && year mod 100 <> 0) || (year mod 400 = 0)
 (*val is_leap_year : int -> bool = <fun>*)
 
-let days_in_month month year =
-  match month with
-  | 1 | 3 | 5 | 7 | 8 | 10 | 12 -> 31 (*For months with 31 days (January, March, May, July, August, October, December).*)
-  | 4 | 6 | 9 | 11 -> 30 (*For months with 30 days (April, June, September, November).*)
-  | 2 -> if is_leap_year year then 29 else 28 (*For February*)
-  | _ -> failwith "Invalid Month."
+let days_in_month month year = match month with
+| 1 | 3 | 5 | 7 | 8 | 10 | 12 -> 31 (*For months with 31 days (January, March, May, July, August, October, December).*)
+| 4 | 6 | 9 | 11 -> 30 (*For months with 30 days (April, June, September, November).*)
+| 2 -> if is_leap_year year then 29 else 28 (*For February*)
+| _ -> failwith "Invalid Month."
 (*val days_in_month : int -> int -> int = <fun>*)
 
 let date2daynum date =
@@ -47,29 +46,28 @@ date2daynum {dayofmonth = 3; monthnum = 3; year = 2024}
 (*2. Write a function weekday_generator:date->weekday->(date->weekday)
 which takes a date and a weekday and returns a function which takes a date and returns the 
 corresponding weekday. Make sure your returned function also works with leap years. *)
+
 let weekday_generator base_date base_weekday =
   let base_daynum = date2daynum base_date in
   let days_diff date =
     let current_daynum = date2daynum date in
     let rec yeargap year year2 = 
-       if year = year2 then 0 
-       else if year < year2 then
-         (if (is_leap_year year) then 366 + yeargap (year+1) year2 
-         else 365 + yeargap (year+1) year2)
-       else
-         (if (is_leap_year year2) then (- 366 + yeargap year (year2 + 1)) 
-         else (- 365 + yeargap year (year2 + 1)))
-
+      if year = year2 then 0 
+      else if year < year2 then
+        (if (is_leap_year year) then 366 + yeargap (year+1) year2 
+        else 365 + yeargap (year+1) year2)
+      else
+        (if (is_leap_year year2) then (- 366 + yeargap year (year2 + 1)) 
+        else (- 365 + yeargap year (year2 + 1)))
     in
     ((current_daynum - base_daynum) + yeargap base_date.year date.year) mod 7
-
   in
   fun current_date ->
     let diff = days_diff current_date in
     let index =
       (match base_weekday with
-       | Mon -> 0 | Tue -> 1 | Wed -> 2 | Thu -> 3 | Fri -> 4 | Sat -> 5 | Sun -> 6
-      ) in
+      | Mon -> 0 | Tue -> 1 | Wed -> 2 | Thu -> 3 | Fri -> 4 | Sat -> 5 | Sun -> 6) 
+    in
     let result = ((index + diff) + 7) mod 7 in
     match result with
     | 0 -> Mon | 1 -> Tue | 2 -> Wed | 3 -> Thu | 4 -> Fri | 5 -> Sat | 6 -> Sun
@@ -95,7 +93,7 @@ let calgen year =
       ()
     else begin
       let weekday = weekday_generator {dayofmonth=1; monthnum=1; year=year} Mon in
-      Hashtbl.add calendar current_date weekday;
+      Hashtbl.add calendar {dayofmonth=current_date.dayofmonth; monthnum=current_date.monthnum; year=current_date.year} weekday;
       let next_date =
         if current_date.dayofmonth < days_in_month current_date.monthnum current_date.year then
           {current_date with dayofmonth = current_date.dayofmonth + 1}
@@ -118,3 +116,4 @@ let calendar_2024 = calgen 2024
 
 let jan_1_2024_weekday = Hashtbl.find calendar_2024 {dayofmonth=1; monthnum=1; year=2024}
 (*val jan_1_2024_weekday : date -> weekday = <fun>*)
+
